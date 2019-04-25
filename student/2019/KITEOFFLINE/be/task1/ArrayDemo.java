7b80a0b71f270edd0053ef3f3041fb485cf2b17b
KITEOFFLINE <44195286+KITEOFFLINE@users.noreply.github.com>
2019-04-25 21:09:30 +0800

    Add files via upload

---
+++ student/2019/KITEOFFLINE/be/task1/ArrayDemo.java
@@ -0,0 +1,8 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] num = {1, 5, 67, 34, 54, 6543, 2356, 545};
+        for (int i = 0; i < num.length; i++){
+            System.out.println(num[i]);
+        }
+    }
+}
