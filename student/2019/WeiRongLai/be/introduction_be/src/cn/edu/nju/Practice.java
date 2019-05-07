15c1cad8f6a2f5593a3b39e8ce5d4432f8050360
ronglai <1135332676@qq.com>
2019-05-08 00:31:51 +0800

    add three new java

---
+++ student/2019/WeiRongLai/be/introduction_be/src/cn/edu/nju/Practice.java
@@ -0,0 +1,16 @@
+package cn.edu.nju;
+
+public class Practice {
+    int add(int a,int b,int c){
+        return a + b + c;
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+}
