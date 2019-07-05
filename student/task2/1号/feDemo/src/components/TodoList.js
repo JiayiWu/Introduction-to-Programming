c934940eb13dd115185cd36ddd772afd3777e9e1
AveryHandsomeBoy <181250078@smail.nju.edu.cn>
2019-07-05 20:07:39 +0800

    Update for group1 fedemo

---
+++ student/task2/1号/feDemo/src/components/TodoList.js
@@ -0,0 +1,30 @@
+import Component from "../libs/Component.js"
+
+class TodoList extends Component {
+  render() {
+    const { data } = this.model
+    return `
+      <header onclick='TodoController.saveData()'>点击保存更改</header>
+      <ol>
+        ${data.todos.map(todo => {
+          if(todo.visible === false)return
+          else return `
+            <li 
+              class='todo ${todo.type==='file'?'file':'dir'} ${todo.visible === true ? '' : 'non-visible'} ${(todo.dir_active===true&&todo.type==='dir') ? 'dir_active' : (data.activeTodoId === todo.id ? 'active' : '')}'
+              onclick='TodoController.clickTodo(${todo.id})'
+            >
+              <div>
+                <div class='title'>${todo.title}</div>
+                <div class='time'>${new Date(todo.createTime).toISOString().split("T")[0]}</div>
+              </div>
+              <div class='content'>${todo.type === 'dir' ? 'dir' : todo.content}</div>
+            </li>
+          `
+        }).join('')}
+      </ol>
+      <footer><p onclick='TodoController.addTodo()'>新建文件</p>/<p onclick='TodoController.addDir()'>新建文件夹</p></footer>
+    `
+  }
+}
+
+export default TodoList
---
+++ student/task2/1号/feDemo/src/controllers/UserController.js
@@ -0,0 +1,101 @@
+import Controller from "../libs/Controller.js";
+import Request from '../libs/Request.js'
+import { todoController } from "../index.js";
+
+class UserController extends Controller {
+  switchMode() {
+    this.model.update(data => ({
+      ...data,
+      isRegister: !data.isRegister
+    }))
+  }
+  async register() {
+    console.log(todoController.fetchTodoList)
+    const form = document.getElementById('loginForm')
+    const formInputs = [...form.getElementsByTagName('input')]
+    let formData = {}
+    formInputs.forEach((input) => {
+      const name = input.getAttribute('name')
+      formData[name] = input.value
+    })
+
+    if(formData['email'] === ''){
+      alert('请输入邮箱地址！')
+      return 
+    }
+
+      //TODO：匹配邮箱是否符合邮箱格式
+    function IsEmail(str) {
+      var reg=/^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/;
+      return reg.test(str);
+    }
+
+    if(IsEmail(formData['email']) !== true){
+      alert('输入的邮箱地址不符合格式！')
+      return 
+    }
+
+    if(formData['password'] === ''){
+      alert('请输入密码！')
+      return 
+    }
+    if (formData['password'] !== formData['repassword']) {
+      alert('两次输入密码不一致！')
+      return
+    }
+
+    const response = await Request.post('/user/account', {
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password
+      }),
+    })
+
+    response.code = 200
+    // 注册成功的判定由前后端商议
+    
+    if (response.code === 200) {
+      console.log('register success')
+      alert('注册成功！')
+      this.model.update(data => ({
+        ...data,
+        isRegister: !data.isRegister
+      }))
+      // 提示注册成功并且跳转到登录，也可以做成注册后自动登录
+    } else {
+      // 处理错误
+      alert(response.data)
+    }
+    return
+  }
+
+  login() {
+    // 获取界面表达数据，请求接口，如果登录成功则关闭弹窗并进入系统
+    // 进入系统后，需要向后端获取todolist并修改model
+    // 需要调用todoController.fetchTodoList
+    let formData = {}
+    const form = document.getElementById('loginForm')
+    const formInputs = [...form.getElementsByTagName('input')]
+    formInputs.forEach((input) => {
+      const name = input.getAttribute('name')
+      formData[name] = input.value
+    })
+    //修改以下请求
+    const response = Request.post('/session/create', {
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password
+      }),
+    })
+    response.code=200
+    TodoController.fetchTodoList()
+    if(response.code === 200){
+      this.model.update(data => ({
+        ...data,
+        user: formData['email']
+      }))
+    }
+  }
+}
+
+export default UserController
